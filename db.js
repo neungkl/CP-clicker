bbfac67b18c0e19a609cf963fb1dac3c50fb36c4
neungkl <kosatelim@gmail.com>
2016-08-02 19:15:39 +0700

    Complete all staff /w testing

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URI || "mongodb://localhost:27017/clicker");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/clicker");
 
 var controlSchema = mongoose.Schema({
   key: String,
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 
-var isProduction = true;
+var isProduction = false;
 
 app.set('port', (process.env.PORT || 8000));
 
@@ -84,6 +84,13 @@ app.post('/login', function(req, res) {
   });
 });
 
+app.get('/logout', function(req, res) {
+  req.session.inGame = false;
+  req.session.inGame = null;
+  res.end();
+});
+
+
 app.get('/ping', function(req, res) {
 
   var send = {
@@ -91,7 +98,7 @@ app.get('/ping', function(req, res) {
   };
 
   db.control.find({}, function(err, data) {
-    if(err) {
+    if(err || typeof data === 'undefined' || data == null) {
       res.send(JSON.stringify(send));
       res.end();
     }
@@ -108,7 +115,7 @@ app.get('/ping', function(req, res) {
 
 app.get('/click', function(req, res) {
   db.control.findOne({ key : 'isPlay'}, function(err, data) {
-    if(err) { res.send('No'); res.end(); }
+    if(err || typeof data === 'undefined' || data == null) { res.send('No'); res.end(); }
 
     if(data.value === 'true') {
       var ins = new db.click({
@@ -129,6 +136,16 @@ app.get('/click', function(req, res) {
   });
 });
 
+app.get('/isReject', function(req, res) {
+  db.user.findOne({ sid: req.sessionID }, function(err, data) {
+    if(err || typeof data === 'undefined' || data == null) {
+      res.send('No'); res.end();
+      return ;
+    }
+    res.send(data.reject ? 'Yes' : 'No'); res.end();
+  });
+});
+
 app.get('/admin', function(req, res) {
   if(req.query.pass === (process.env.ADMIN_PASSWORD || '1234')) {
     req.session.admin = true;
@@ -142,7 +159,7 @@ app.get('/admin', function(req, res) {
   }
 });
 
-app.get('/admin/*', function(req, res, next) {
+app.all('/admin/*', function(req, res, next) {
   if(typeof req.session.admin === 'undefined' || req.session.admin == null || req.session.admin === false) {
     res.writeHead(302, {'Location': '../index'});
     res.end();
@@ -157,6 +174,11 @@ app.get('/admin/data', function(req, res) {
 
     db.click.find({}, function(err2, clickData) {
       if(err2) { res.send('[]'); res.end(); }
+
+      if(typeof userData === 'undefined' || userData == null ||
+         typeof clickData === 'undefined' || clickData == null) {
+        res.send('[]'); res.end();
+      }
 
       var ret = [];
 
@@ -181,6 +203,46 @@ app.get('/admin/data', function(req, res) {
     });
   });
 });
+
+app.post('/admin/updateReject', function(req, res) {
+  db.user.findOne({ sid: req.body.sid }, function (err, doc) {
+    if(err || typeof doc === 'undefined' || doc == null) {
+      res.send('Error'); res.end();
+    }
+
+    doc.reject = req.body.reject;
+    doc.save(function(err) {
+      if(err) { res.send('Error'); res.end(); }
+
+      res.send('{"success": true}'); res.end();
+    });
+  });
+});
+
+app.post('/admin/updateControl', function(req, res) {
+  db.control.findOne({ key: req.body.type }, function (err, doc) {
+    if(err || typeof doc === 'undefined' || doc == null) {
+      res.send('Error'); res.end();
+    }
+
+    doc.value = req.body.val;
+    doc.save(function(err) {
+      if(err) { res.send('Error'); res.end(); }
+
+      res.send('{"success": true}'); res.end();
+    });
+  });
+});
+
+app.post('/admin/clean', function(req, res) {
+  db.click.remove({}, function(err) {
+    if(err) { res.send('Error'); res.end(); }
+    else {
+      res.send('{"success": true}'); res.end();
+    }
+  })
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
